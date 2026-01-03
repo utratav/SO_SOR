d897998cfb57909dd2d49d522d4a23badf46f967
Hugo Utrata <hugoutrata@icloud.com>
2026-01-03 05:49:11 +0100

    nieobsluzona kolejka 2, miejsce na msgrcv od lekarza, wyjscie z poczekalni

--- pacjent.c
+++ pacjent.c
@@ -64,28 +64,49 @@ int main(int argc, char *argv[])
     stan->liczba_pacjentow_w_srodku++;
     stan->dlugosc_kolejki_rejestracji++;
     int aktualna_kolejka = stan->dlugosc_kolejki_rejestracji;
-    semop(semid, &mutex_unlock);
+    semop(semid, &mutex_unlock, 1);
 
-    
+    if (aktualna_kolejka > LIMIT_KOLEJKI_K)
+    {
+        semop(semid, &mutex_lock, 1);
+        if (stan->czy_okienko_2_otwarte == 0)
+        {
+            stan->czy_okienko_2_otwarte = 1;
+
+            //ODPAL DRUGIE OKIENKO()
+        }
+        semop(semid, &mutex_unlock, 1);
+    }
+
+    KomunikatPacjenta msg;
+    msg.mtype = 1;
+    msg.pacjent_pid = mpid;
+
+    msg.czy_vip = vip;
+    sprintf(msg.opis_objawow, "objaw");
+
+    if (msgsnd(&msg, sizeof(msg) - sizeof(long), 0) == -1)
+    {
+        perror("blad wysylania do rejestru");
+    }
 
 
 
+    sleep(10); // tutaj pacjent bedzie czekac na lekarza msgrcv()
 
+    semop(semid, &mutex_lock, 1);
+    stan->liczba_pacjentow_w_srodku--;
+    semop(semid, &mutex_unlock, 1);
+    shmdt(stan);
 
+    struct sembuf wyjscie_z_poczekalni;
+    wyjscie_z_poczekalni.sem_num = SEM_MIEJSCA_SOR; 
+    wyjscie_z_poczekalni.sem_op = 1;
+    wyjscie_z_poczekalni.sem_flg = 0;
 
+    semop(semid, &wyjscie_z_poczekalni, 1);
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 
 
 }
